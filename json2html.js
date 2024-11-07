72fbe965a4be108283489f1cfc8445052e4b74d7
Bathula Karthikender Reddy <119797959+karthikbathula@users.noreply.github.com>
2024-11-07 18:51:35 +0530

    Add json2html.js

---
+++ json2html.js
@@ -0,0 +1,17 @@
+export default function json2html(data) {
+  let html = '<table data-user="karthikbathula9@gmail.com">';
+  html += '<thead><tr>';
+  const headers = ["Name", "Age", "Gender"];
+  headers.forEach(header => html += `<th>${header}</th>`);
+  html += '</tr></thead>';
+  html += '<tbody>';
+  data.forEach(row => {
+    html += '<tr>';
+    headers.forEach(header => {
+      html += `<td>${row[header] || ''}</td>`; 
+    });
+    html += '</tr>';
+  });
+  html += '</tbody></table>';
+  return html;
+}
